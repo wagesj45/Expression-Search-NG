37f677adf0f98febdb3b774766e8dfcc1e69cbda
Jordan Wages <jordan@jordanwages.com>
2025-06-14 16:01:17 -0500

    Removing base/template add-on

--- expression-search-ng/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")"
-zip -r extension.xpi _locales api html modules scripts skin ex-background.js manifest.json ai-filter > /dev/null
-echo "Created extension.xpi"
